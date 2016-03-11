3832809ea65002b770b0819bbb19c91029d2daf1
Rafael F. Silva <rafaelfsilva1@gmail.com>
2016-03-11 12:33:01 -0300

    specify better the semver of php

--- setup-php5.sh
+++ setup-php5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Updating PHP 5 repository"
+echo "Updating PHP 5.6 repository"
 apt-get install python-software-properties build-essential -y > /dev/null
 add-apt-repository ppa:ondrej/php5-5.6 -y > /dev/null
 apt-get update > /dev/null
 
-echo "Installing PHP 5"
+echo "Installing PHP 5.6"
 apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
 
-echo "Installing PHP 5 extensions"
+echo "Installing PHP 5.6 extensions"
 apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql php5-intl -y > /dev/null
--- setup-php7.sh
+++ setup-php7.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Updating PHP 7 repository"
+echo "Updating PHP 7.0 repository"
 apt-get install python-software-properties build-essential -y
 add-apt-repository ppa:ondrej/php -y
 apt-get update
 
-echo "Installing PHP 7"
+echo "Installing PHP 7.0"
 apt-get install php7.0-common php7.0-cli php7.0-fpm -y
 
-echo "Installing PHP 7 extensions"
+echo "Installing PHP 7.0 extensions"
 apt-get install curl php7.0-curl php7.0-gd php7.0-mcrypt php7.0-mysql php7.0-intl -y
