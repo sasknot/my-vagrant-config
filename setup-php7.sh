d76d43f9ddad31e9f05227f9dbeeb9122ae1f161
Rafael F. Silva <rafaelfsilva1@gmail.com>
2016-08-02 20:21:48 -0300

    refactor of php provisioners supress outputs, better composer installation, include nginx config inside script, fix some php extensions not being installed

--- setup-php7.sh
+++ setup-php7.sh
@@ -1,34 +1,84 @@
 #!/bin/bash
 
-echo "Installing Nginx"
-apt-get install nginx -y
-
+# MySQL 5.6
 echo "Preparing MySQL"
-apt-get install debconf-utils -y
+apt-get install debconf-utils -y >/dev/null
 debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpwd"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpwd"
 
 echo "Installing MySQL"
-apt-get install mysql-server-5.6 mysql-client-5.6 -y
+apt-get install mysql-server-5.6 mysql-client-5.6 -y >/dev/null
 
-echo "Installing Composer"
-wget 'https://getcomposer.org/composer.phar'
-chmod 755 composer.phar
-mv composer.phar /usr/local/bin/composer
+
+# Nginx (latest)
+echo "Installing Nginx"
+apt-get install nginx -y >/dev/null
 
 echo "Configuring Nginx"
-cp /var/www/nginx_vhost_php7 /etc/nginx/sites-available/vhosts
+cat > /etc/nginx/sites-available/vhosts <<'EOL'
+server {
+  listen 80;
+  server_name localhost;
+
+  root /var/www/;
+  index index.php index.html;
+
+  # Important for VirtualBox
+  sendfile off;
+
+  location / {
+    try_files $uri $uri/ =404;
+  }
+
+  location ~* \.php {
+    include fastcgi_params;
+
+    fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    fastcgi_index index.php;
+    fastcgi_cache off;
+  }
+}
+
+include /var/www/**/.nginx-php7.conf;
+EOL
 ln -s /etc/nginx/sites-available/vhosts /etc/nginx/sites-enabled/vhosts
 rm -rf /etc/nginx/sites-enabled/default
 service nginx restart
 
+
+# PHP 7.0 (with cli, fpm, curl, gd, mcrypt, mysql, intl)
 echo "Updating PHP 7.0 repository"
-apt-get install python-software-properties build-essential -y
-add-apt-repository ppa:ondrej/php -y
-apt-get update
+apt-get install python-software-properties build-essential -y >/dev/null
+add-apt-repository ppa:ondrej/php -y >/dev/null
+apt-get update >/dev/null
 
 echo "Installing PHP 7.0"
-apt-get install php7.0-common php7.0-cli php7.0-fpm -y
+apt-get install php7.0-common php7.0-cli php7.0-fpm -y >/dev/null
+
+echo "Installing curl"
+apt-get install curl -y >/dev/null
 
 echo "Installing PHP 7.0 extensions"
-apt-get install curl php7.0-curl php7.0-gd php7.0-mcrypt php7.0-mysql php7.0-intl -y
+apt-get install php7.0-curl php7.0-gd php7.0-mcrypt php7.0-mysql php7.0-intl php7.0-mbstring -y >/dev/null
+
+
+# Composer (latest)
+echo "Installing Composer"
+COMPOSER_EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+COMPOSER_ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$COMPOSER_EXPECTED_SIGNATURE" = "$COMPOSER_ACTUAL_SIGNATURE" ]
+then
+    php composer-setup.php --quiet
+    RESULT=$?
+    rm composer-setup.php
+    exit $RESULT
+else
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+mv composer.phar /usr/local/bin/composer
