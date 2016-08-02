00836880c7ec26d90d5a8ed49c114cbe4a26a4ca
Rafael F. Silva <rafaelfsilva1@gmail.com>
2016-08-02 20:17:44 -0300

    remove git and supress output messages

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 echo "Provisioning virtual machine..."
+apt-get update >/dev/null
 
 echo "Installing language"
-apt-get install language-pack-en -y > /dev/null
+apt-get install language-pack-en -y >/dev/null
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
-
-echo "Installing git"
-apt-get install git-all -y > /dev/null
